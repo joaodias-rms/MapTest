b32ba364d41aff242e35436d656e306acaf968f3
joao <joao.victor@topsapp.com.br>
2021-09-16 10:08:41 -0400

    ajuste de dependências

--- android/app/src/main/java/com/uber/MainApplication.java
+++ android/app/src/main/java/com/uber/MainApplication.java
@@ -26,8 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
-            new MapsPackage(),
             new MapsPackage()
       );
     }
